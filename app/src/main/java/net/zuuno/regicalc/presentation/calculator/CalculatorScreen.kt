cb2e2941039d45db7c7d089fc8d2700207938ecd
nozoo1025 <nozoo1025@gmail.com>
2023-03-09 00:14:15 +0900

    add: 計算機の画面を作成

---
+++ app/src/main/java/net/zuuno/regicalc/presentation/calculator/CalculatorScreen.kt
@@ -0,0 +1,56 @@
+package net.zuuno.regicalc.presentation.calculator
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import net.zuuno.regicalc.presentation.calculator.components.CalculatorDisplay
+import net.zuuno.regicalc.presentation.calculator.components.CalculatorKeypad
+import net.zuuno.regicalc.presentation.calculator.components.CalculatorResult
+import net.zuuno.regicalc.presentation.calculator.components.CalculatorShoppingList
+
+@Composable
+fun CalculatorScreen() {
+    val viewModel = viewModel<CalculatorViewModel>()
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CalculatorResult(
+            totalPrice = viewModel.uiState.totalPrice,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = 16.dp
+                )
+        )
+        CalculatorShoppingList(
+            shoppingList = viewModel.uiState.shoppingList,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = 16.dp
+                ),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            CalculatorDisplay(
+                modifier = Modifier.fillMaxWidth(),
+                price = viewModel.uiState.price,
+                quantity = viewModel.uiState.quantity,
+                operation = viewModel.uiState.operation
+            )
+            CalculatorKeypad(
+                modifier = Modifier.fillMaxWidth(),
+                onAction = { action ->
+                    viewModel.onAction(action)
+                }
+            )
+        }
+    }
+}
