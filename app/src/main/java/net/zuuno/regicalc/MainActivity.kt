f5a228053ba78c92d8f712935cf3c1d53821d5bc
nozoo1025 <nozoo1025@gmail.com>
2023-03-09 00:16:19 +0900

    update: CalculatorScreenを表示するようにMainActivityを変更

--- app/src/main/java/net/zuuno/regicalc/MainActivity.kt
+++ app/src/main/java/net/zuuno/regicalc/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import net.zuuno.regicalc.presentation.calculator.CalculatorScreen
 import net.zuuno.regicalc.ui.theme.RegicalcTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CalculatorScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RegicalcTheme {
-        Greeting("Android")
     }
 }
