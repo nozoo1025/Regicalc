c8fe825d340f1ad94c1d03077978969c887f0f71
nozoo1025 <nozoo1025@gmail.com>
2023-03-09 00:09:09 +0900

    add: 買い物の合計金額を表示するUIを作成

---
+++ app/src/main/java/net/zuuno/regicalc/presentation/calculator/components/CalculatorResult.kt
@@ -0,0 +1,47 @@
+package net.zuuno.regicalc.presentation.calculator.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CalculatorResult(
+    totalPrice: Double,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Text(
+                text = "合計",
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = "(税込)",
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+        Row {
+            Text(
+                text = "¥ $totalPrice",
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.displayMedium
+            )
+        }
+    }
+}
